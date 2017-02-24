74ac61a5b5d503e11cdb5e8cb6426f6228a9f037
Grzegorz.Kruk <cr00q.pl@gmail.com>
2017-02-24 13:24:24 +0100

    add photos, contact form
    add photos, contact form

---
+++ contact.php
@@ -0,0 +1,56 @@
+<?php
+
+define( 'ADMIN_EMAIL', 'cr00q.pl@gmail.com' );
+define( 'ADMIN_NAME', 'Grzegorz Kruk' );
+
+//Jaka metoda zaladowlismy plik mailSender.php
+if( $_SERVER['REQUEST_METHOD'] === 'POST' ){
+
+    //Zaladowanie biblioteki PHPMailer
+    require_once 'PHPMailer/PHPMailerAutoload.php';
+
+    //Walidacja pol formularza
+    $name = isset( $_POST['name'] ) ? trim($_POST['name']) : '';    
+    $surname = isset( $_POST['surname'] ) ? trim($_POST['surname']) : '';    
+    $email = isset( $_POST['email'] ) ? trim($_POST['email']) : '';    
+    $phone = isset( $_POST['phone'] ) ? trim($_POST['phone']) : '';    
+    $subject = isset( $_POST['subject'] ) ? trim($_POST['subject']) : '';    
+    $message = isset( $_POST['message'] ) ? trim($_POST['message']) : '';
+
+    if( !empty($name) && !empty($surname) && !empty($email) && !empty($subject) && !empty($message) ){
+        $subject = "Formularz ze strony SIGNALEN - $subject";
+        $message = "Wiadomość od $name $surname ($email, $phone)<br>Treść wiadomości:<br> $message";
+    }
+
+    $mail = new PHPMailer;
+
+    // Set up SMTP  
+    $mail->IsSMTP();                // Sets up a SMTP connection  
+    $mail->SMTPAuth = true;         // Connection with the SMTP does require authorization    
+    $mail->SMTPSecure = "ssl";      // Connect using a TLS connection  
+    $mail->Host = "ssl0.ovh.net";  //Gmail SMTP server address
+    $mail->Port = 465;  //Gmail SMTP port
+//    $mail->Encoding = '7bit';
+
+    // Authentication  
+    $mail->Username   = "kontakt@uzit.pl"; // Your full Gmail address
+    $mail->Password   = "Cr00qCr00q"; // Your Gmail password
+
+    $mail->setFrom( $email );
+    $mail->addAddress( ADMIN_EMAIL, ADMIN_NAME );
+    $mail->addReplyTo( $email, $name, $surname );
+    $mail->isHTML(true);
+    $mail->Subject = $subject;
+    $mail->Body = $message;
+    $mail->CharSet = 'UTF-8';
+
+    if(!$mail->send()){
+        $msg = urlencode( 'Błąd w wysyłaniu wiadomości: ' . $mail->ErrorInfo );
+        $mode = 'error';
+    } else {
+        $msg = urlencode( 'Wiadomość została wysłana' );
+        $mode = 'success';
+    }
+
+    header( 'Location: kontakt.html?msg='. $msg . '&mode='. $mode );
+}
